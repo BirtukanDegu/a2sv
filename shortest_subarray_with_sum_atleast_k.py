f14d87c5703858a4176e84689a7a63fbf2fef555
birtukan <birtukandegu2127@gmail.com>
2022-05-15 17:23:41 +0300

    shortest_subarray_with_sum_atleast_k

---
+++ shortest_subarray_with_sum_atleast_k.py
@@ -0,0 +1,11 @@
+dq = [(-1, 0)]
+        sums, res = 0, float("inf")
+        for i, val in enumerate(nums):
+            sums += val
+            while dq and sums-dq[0][1] >= k:
+                res = min(res, i-dq[0][0])
+                dq.pop(0)
+            while dq and sums < dq[-1][1]:
+                dq.pop()
+            dq.append((i, sums))
+        return -1 if res == float("inf") else res
