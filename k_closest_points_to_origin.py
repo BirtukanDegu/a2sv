2cd12e3bcfda75201becf578c9bcec693bca0295
birtukan <birtukandegu2127@gmail.com>
2022-05-17 10:15:29 +0300

    k_closest_points_to_origin

--- k_closest_points_to_origin.py
+++ k_closest_points_to_origin.py
@@ -1,16 +1,12 @@
-#import math
-#from array import *
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        newarray = []
-        for i in range(len(points)):
-            newarray.append(points[i][0]**2 + points[i][-1]**2)
-        newar = sorted(newarray)
-        newar2 = newar[:k]
-        maximum = newar2[k-1]
-        newarray2 = []
-        for j in range(len(points)):
-            if (points[j][0]**2 + points[j][1]**2)<=maximum:
-                newarray2.append(points[j])
-        newarray3 = newarray2[:k]
-        return newarray3
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        res=[]
+        d={}
+        for i in points:
+            d[math.sqrt((i[0]**2)+(i[1]**2))]=d.get(math.sqrt((i[0]**2)+(i[1]**2)),[])+[i]
+        sorted_d=sorted(d.keys())
+        for i in sorted_d[:K]:
+            if len(res)<K:
+                res.extend(d[i])
+            else:break
+        return res
